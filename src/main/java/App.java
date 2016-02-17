0d1125195bc9b8e3fd0c53478f6653acd6f41497
isabelle george and trevor elvey <trevor.elvey@gmail.com>
2016-02-17 14:14:22 -0800

    pass first integration test on landing page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,15 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+    staticFileLocation("/public");
+      String layout = "templates/layout.vtl";
 
+      get("/", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        model.put("cds", request.session().attribute("cds"));
+        model.put("template", "templates/index.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
 
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -20,5 +20,9 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-  // @Test
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("*~mY cDz~*");
+  }
 }
